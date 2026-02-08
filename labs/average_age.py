dafd5817557ae817bd38417d8a67104b0f6cd0bf
monkj24 <j.mnich76@gmail.com>
2026-02-08 20:06:46 +0000

    average

--- labs/average_age.py
+++ labs/average_age.py
@@ -12,8 +12,23 @@ with open (DATADIR + FILENAME, "rt") as fp:
     total = 0 
     for line in reader: 
         if not linecount: # first row ie header row 
-            linecount += 1
+            pass
         else: # all subsequent rows 
             total += int(line[1]) # why 1 
+        linecount += 1 
+    print (f"average is {total/(linecount-1)}") # why -1 ? 
+
+# Use the quote parameter to read in the numbers as floats 
+
+with open (DATADIR + FILENAME, "rt") as fp: 
+    reader = csv.reader(fp, delimiter=",", quoting=csv.QUOTE_NONNUMERIC) 
+    linecount = 0 
+    total = 0 
+    for line in reader: 
+        if not linecount: # first row ie header row 
+            pass 
+        else: # all subsequent rows 
+            total += line[1] # why 1 
+ 
         linecount += 1 
     print (f"average is {total/(linecount-1)}") # why -1 ? 
