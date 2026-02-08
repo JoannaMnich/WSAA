0d421baca5b87f7096fee1b32224040cb778721e
monkj24 <j.mnich76@gmail.com>
2026-02-08 20:24:00 +0000

    bank_holidays

--- labs/show_json.py
+++ labs/show_json.py
@@ -3,4 +3,6 @@ import requests
 url =" https://www.gov.uk/bank-holidays.json" 
 response = requests.get(url) 
 data = response.json() 
-print(data) 
+# print(data) 
+
+print(data['northern-ireland']['events'][0]) 
