aa568f038c3dd4fa143328070bd44908766c2261
monkj24 <j.mnich76@gmail.com>
2026-02-28 21:46:38 +0000

    file deleted

--- Assignments/labs/show_json.py
+++
@@ -1,8 +0,0 @@
-import requests 
- 
-url =" https://www.gov.uk/bank-holidays.json" 
-response = requests.get(url) 
-data = response.json() 
-# print(data) 
-
-print(data['northern-ireland']['events'][0]) 
